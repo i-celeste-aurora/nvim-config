1e684ab5af2a828ead2e9e55789e8c5bd0ee4169
Imdat Celeste Solak <imdat@solak.de>
2025-08-08 12:55:50 +0200

    sql

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -15,6 +15,9 @@ require("conform").setup {
     json = { "jq", lsp_format = "fallback" },
     dart = { "dart_format", stop_after_first = true },
     nginx = { "nginxfmt", stop_after_first = true },
+    sql = { "pg_format", stop_after_first = true },
+    postgresql = { "pg_format", stop_after_first = true },
+    pgsql = { "pg_format", stop_after_first = true },
     format_on_save = {
       -- I recommend these options. See :help conform.format for details.
       lsp_format = "fallback",
